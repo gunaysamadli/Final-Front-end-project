3610125636b08b5b98b4dd07c18ca239f0dc480b
GunaySamadli <gunayfs@code.edu.az>
2021-11-24 22:05:17 +0400

    Added Color Filter in Shop Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -281,3 +281,65 @@ SearchBarX.addEventListener("click", function (e) {
     search.style.display = "none"
 })
 
+
+// Filter Color
+
+
+const tabColors = Array.from(document.querySelectorAll(".tab-color"));
+const tabColorContents = Array.from(document.querySelectorAll(".content-colors .content-color"));
+
+const clearColorActives = function (dataId) {
+    tabColors.forEach(tabREw => {
+        if (tabREw.parentElement.parentElement.getAttribute('data-id') == dataId) {
+            tabREw.classList.remove("active");
+        }
+    });
+    tabColorContents.forEach(tabRewContent => {
+        if (tabRewContent.parentElement.getAttribute('id') == dataId) {
+            tabRewContent.classList.remove("active");
+        }
+    });
+}
+
+
+
+tabColors.forEach(tabREw => {
+    tabREw.onclick = function () {
+        let dataId = this.parentElement.parentElement.getAttribute('data-id');
+        let contentColor = document.getElementById(dataId);
+        clearColorActives(dataId);
+
+        const targetId = tabREw.getAttribute("data-target");
+        const targetContent = contentColor.querySelector('#' + targetId)
+        tabREw.classList.add("active");
+        targetContent.classList.add("active")
+    }
+})
+
+
+
+//Timer
+// const countdown = () => {
+//     const countDate = new Date('November 26,2022 00:00:00').getTime();
+//     const now = new Date().getTime();
+//     const gap = countDate - now;
+
+
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24;
+
+//     //Calculate
+//     const textDay = Math.floor(gap / day);
+//     const textHour = Math.floor((gap % day) / hour);
+//     const textMinute = Math.floor((gap % hour) / minute);
+//     const textSEcond = Math.floor((gap % minute) / second);
+
+//     document.querySelector(".day").innerText = textDay;
+//     document.querySelector(".hour").innerText = textHour;
+//     document.querySelector(".minute").innerText = textMinute;
+//     document.querySelector(".second").innerText = textSEcond;
+
+// };
+// setInterval(countdown, 1000);
